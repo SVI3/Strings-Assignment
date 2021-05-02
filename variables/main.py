abf888a92018a59f5198f14c9b561cb1f10e8559
SVI3 <82755620+SVI3@users.noreply.github.com>
2021-05-02 16:52:32 +0200

    Add files via upload

---
+++ variables/main.py
@@ -0,0 +1,13 @@
+# Do not modify these lines
+__winc_id__ = '7152c06aa3ac4d5f964ca8619ecb7e8f'
+__human_name__ = 'variables'
+
+# Add your code after this line
+
+A = str('This is an example of a string.')
+
+B = int(3)
+
+C = bool(True)
+
+print ('', A, '', B, '', C, sep='\n')
